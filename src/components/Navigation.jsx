0c3c26b9ada6dbee0669188ae9175ecced4930aa
strwbrry05 <jq.strwbrry05@gmail.com>
2025-06-19 11:37:18 -0700

    tentative nav complete

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = () => {
       >
         <div
           className="flex justify-center md:justify-evenly items-center 
-        xl:text-center xl:w-[1000px]"
+        xl:text-center xl:w-[1200px]"
         >
           <button className="cursor-pointer md:hidden">
             <HiMenu
@@ -62,7 +62,7 @@ const Navigation = () => {
 
           <h1
             className="
-          cursor-pointer text-center mr-[2.5em] md:mr-[0]
+          cursor-pointer text-center mr-[2em] md:mr-[0]
         text-(--color-white) font-(family-name:--font-title) 
       font-black text-[2rem] leading-[0.75]"
           >
@@ -101,39 +101,39 @@ const Navigation = () => {
             </ul>
           </div>
         </div>
+      </div>
 
-        <div
-          className={`
+      <div
+        className={`
     ${
       isActive
         ? "translate-y-[0vh] transition transform duration-500 ease-in-out"
         : "translate-y-[-100vh] transition transform duration-500 ease-in"
     }
 
-    md:hidden z-4
+    md:hidden z-4 w-full
     bg-(--color-black) text-(--color-gray) font-(family-name:--font-text)
     font-light pt-[1.25em] pb-[1.25em]
     flex justify-center flex-col items-center shadow-[0_3px_3px_rgba(0,0,0,0.5)]`}
-        >
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            Home
-          </h2>
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            Members
-          </h2>
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            Discography
-          </h2>
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            Photocards
-          </h2>
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            Q&A
-          </h2>
-          <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
-            YouTube
-          </h2>
-        </div>
+      >
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          Home
+        </h2>
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          Members
+        </h2>
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          Discography
+        </h2>
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          Photocards
+        </h2>
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          Q&A
+        </h2>
+        <h2 className="mb-[1em] cursor-pointer hover:font-semibold hover:text-(--color-white) hover:scale-110">
+          YouTube
+        </h2>
       </div>
     </>
   );
