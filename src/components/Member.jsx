679bb2151a838b47cd2b92a5a01f7a9dc812d534
strwbrry05 <jq.strwbrry05@gmail.com>
2025-06-25 22:28:06 -0700

    tentative member layout

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import lm_renjunFull from "../assets/images/lightMode/lm_renjunFull.png";
 
 const Member = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", resize);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, [windowWidth]);
+
   return (
     <div className="pb-[3em]">
       <div
         className="grid grid-cols-6 grid-rows-6
-      
-      w-[480px] m-auto md:bg-blue-200 lg:bg-blue-400"
+      lg:hidden
+      w-[480px] m-auto"
       >
         <h2
           className="col-start-1 col-end-7 row-start-1 row-end-3 justify-self-end uppercase z-2
@@ -36,7 +50,7 @@ const Member = () => {
           <div className=" member_mobileTopRightTriangle"></div>
         </div>
       </div>
-      <div className="w-[480px] m-auto">
+      <div className="w-[480px] m-auto lg:hidden">
         <p className="w-[90%] m-auto font-(family-name:--font-text) mb-[1.5em]">
           Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
           faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
@@ -47,6 +61,70 @@ const Member = () => {
           per conubia nostra inceptos himenaeos.
         </p>
         <div className="w-[90%] h-[200px] bg-(--color-red) m-auto opacity-[70%]"></div>
+      </div>
+
+      <div className="hidden lg:block lg:w-[950px] xl:w-[1200px] m-auto">
+        <div className="grid grid-cols-12 grid-rows-12 xl:grid-rows-10">
+          <h2
+            className="z-2 col-start-1 col-end-7 row-start-1 row-end-3 ml-[0.25em]
+          uppercase text-(--color-red) font-(family-name:--font-solid) 
+          text-[6.5rem] text-shadow-[4px_6px_0px_rgb(0,0,0,0.9)] tracking-wider
+          
+          xl:text-[7rem] xl:tracking-widest xl:ml-[0.45em]"
+          >
+            renjun
+          </h2>
+          <img
+            src={lm_renjunFull}
+            alt=""
+            className="z-2 col-start-7 col-end-13 row-start-2 row-end-10
+            w-[500px] justify-self-start self-center xl:justify-self-center
+            
+            xl:row-start-1 xl:row-end-9"
+          />
+          <div
+            className="z-2 col-start-1 col-end-7 row-start-3 row-end-4 self-center mr-[5em]
+        flex justify-evenly font-(family-name:--font-text) font-bold
+        
+        xl:row-start-2 xl:mr-[7em]"
+          >
+            <p>Month 00 2000</p>
+            <p>SIGN</p> <p>Blood Type</p>
+          </div>
+          <p
+            className="z-2 col-start-1 col-end-7 row-start-4 row-end-10 
+          font-(family-name:--font-text) text-[1.15rem] w-[90%] leading-9
+          justify-self-center self-center
+          
+          xl:w-[80%] xl:self-center xl:text-[1.05rem]
+          xl:row-start-3 xl:row-end-9 xl:col-end-8 xl:mb-[4em]"
+          >
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
+            tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+            Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+            hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
+            tempor.
+          </p>
+          <div
+            className={`z-1 ${
+              windowWidth >= 1280
+                ? "member_fullTopRightTriangle"
+                : "member_desktopTopRightTriangle"
+            }
+          col-start-6 col-end-13 row-start-1 row-end-13
+          xl:row-end-11`}
+          ></div>
+          <div
+            className="z-2 col-start-1 col-end-11 row-start-10 row-end-13
+          w-[95%] h-[200px] bg-(--color-red) m-auto opacity-[70%]
+          
+          xl:row-start-8 xl:row-end-11 xl:h-[225px] xl:w-[90%]"
+          ></div>
+        </div>
       </div>
     </div>
   );
