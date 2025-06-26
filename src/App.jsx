af5db698733f652e53d69d82d89bdb1913fd1e3c
strwbrry05 <jq.strwbrry05@gmail.com>
2025-06-26 00:59:52 -0700

    tentative 3 members

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import { useState } from "react";
 import QA from "./components/QA";
 import Member from "./components/Member";
 
+import lm_renjunFull from "./assets/images/lightMode/lm_renjunFull.png";
+import dm_renjunFull from "./assets/images/darkMode/dm_renjunFull.png";
+import lm_haechanFull from "./assets/images/lightMode/lm_haechanFull.png";
+import dm_haechanFull from "./assets/images/darkMode/dm_haechanFull.png";
+import lm_chenleFull from "./assets/images/lightMode/lm_chenleFull.png";
+import dm_chenleFull from "./assets/images/darkMode/dm_chenleFull.png";
+
 function App() {
   const [isDark, setIsDark] = useState(false);
-
-  // const toggleViewMode = () => {
-  //   setIsDark(!isDark);
-  // };
 
   const toggleDark = () => {
     setIsDark(true);
@@ -29,7 +32,7 @@ function App() {
         <Navigation isDark={isDark} />
       </div>
 
-      <div className="m-5 flex justify-center">
+      <div className="m-5 flex justify-center cursor-pointer">
         <div
           className="bg-(--color-dark-gray) flex items-center justify-center w-[100px] h-[40px]
         rounded-[3rem] "
@@ -54,14 +57,87 @@ function App() {
         </div>
       </div>
 
+      {/* RENJUN */}
       <div
         className={`${
           isDark ? "bg-(--color-dark-blue) text-(--color-white)" : ""
         }`}
       >
-        <Member isDark={isDark} />
-        <Member />
-        <Member />
+        <Member
+          isDark={isDark}
+          color={isDark ? "text-(--color-red)" : "text-(--color-red)"}
+          scrollColor={isDark ? "bg-(--color-member-dark)" : "bg-(--color-red)"}
+          name={"renjun"}
+          birthday={"March 23 2000"}
+          zodiac={"Aries"}
+          blood={"O"}
+          IMG={isDark ? dm_renjunFull : lm_renjunFull}
+          mobileW={isDark ? "w-[300px]" : "w-[350px]"}
+          fullW={isDark ? "w-[380px]" : "w-[500px]"}
+          mobileT={"member_mobileRedTopRightTriangle"}
+          desktopT={"member_desktopRedTopRightTriangle"}
+          fullT={"member_fullRedTopRightTriangle"}
+        />
+      </div>
+
+      {/* HAECHAN */}
+      <div
+        className={`${isDark ? "bg-(--color-blue) text-(--color-white)" : ""}`}
+      >
+        <Member
+          isDark={isDark}
+          color={isDark ? "text-(--color-yellow)" : "text-(--color-yellow)"}
+          scrollColor={
+            isDark ? "bg-(--color-member-light)" : "bg-(--color-yellow)"
+          }
+          name={"haechan"}
+          birthday={"June 06 2000"}
+          zodiac={"Gemini"}
+          blood={"AB"}
+          IMG={isDark ? dm_haechanFull : lm_haechanFull}
+          mobileW={isDark ? "w-[300px]" : "w-[315px]"}
+          fullW={isDark ? "w-[380px]" : "w-[400px]"}
+          mobileT={"member_mobileYellowBottomRightTriangle"}
+          desktopT={"member_desktopYellowTopRightTriangle"}
+          fullT={"member_fullYellowTopRightTriangle"}
+        />
+      </div>
+
+      {/* CHENLE */}
+      <div
+        className={`${
+          isDark ? "bg-(--color-dark-blue) text-(--color-white)" : ""
+        }`}
+      >
+        <Member
+          isDark={isDark}
+          color={isDark ? "text-(--color-dark-red)" : "text-(--color-blue)"}
+          scrollColor={
+            isDark ? "bg-(--color-member-dark)" : "bg-(--color-blue)"
+          }
+          name={"chenle"}
+          birthday={"November 22 2001"}
+          zodiac={"Sagittarius"}
+          blood={"A"}
+          IMG={isDark ? dm_chenleFull : lm_chenleFull}
+          mobileW={isDark ? "w-[300px]" : "w-[265px]"}
+          fullW={isDark ? "w-[380px]" : "w-[370px]"}
+          mobileT={
+            isDark
+              ? "member_mobileDRedTopRightTriangle"
+              : "member_mobileBlueTopRightTriangle"
+          }
+          desktopT={
+            isDark
+              ? "member_desktopDRedTopRightTriangle"
+              : "member_desktopBlueTopRightTriangle"
+          }
+          fullT={
+            isDark
+              ? "member_fullDRedTopRightTriangle"
+              : "member_fullBlueTopRightTriangle"
+          }
+        />
       </div>
 
       <div className={`bg-(--color-red)`}>
--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import lm_renjunFull from "../assets/images/lightMode/lm_renjunFull.png";
-import dm_renjunFull from "../assets/images/darkMode/dm_renjunFull.png";
 
 const Member = (props) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -18,7 +16,7 @@ const Member = (props) => {
   }, [windowWidth]);
 
   return (
-    <div className="pb-[3em]">
+    <div className="pt-[1.25em] pb-[1.5em]">
       <div
         className="grid grid-cols-6 grid-rows-6
       lg:hidden
@@ -26,30 +24,30 @@ const Member = (props) => {
       >
         <h2
           className="col-start-1 col-end-7 row-start-1 row-end-3 justify-self-end uppercase z-2
-            mr-[0.25em] mb-[1em] self-center
-          font-(family-name:--font-solid) text-[5rem] text-(--color-white) text-shadow-[4px_6px_0px_rgb(0,0,0,1)]"
+        mr-[0.25em] mb-[1em] self-center
+        font-(family-name:--font-solid) text-[5rem] text-(--color-white) text-shadow-[4px_6px_0px_rgb(0,0,0,1)]"
         >
-          Renjun
+          {props.name}
         </h2>
         <img
-          src={props.isDark ? dm_renjunFull : lm_renjunFull}
-          alt="light mode renjun"
+          src={props.IMG}
+          alt={props.name}
           className={`z-2 col-start-1 col-end-7 row-start-2 row-end-6
           self-center justify-self-center
-          ${props.isDark ? "w-[300px]" : "w-[350px]"}`}
+          ${props.mobileW}`}
         />
         <div
           className="col-start-1 col-end-7 row-start-6 row-end-7 self-start mt-[1em]
-        flex justify-evenly font-(family-name:--font-text)"
+        flex justify-evenly font-(family-name:--font-text) font-bold"
         >
-          <p>Month 00 2000</p>
-          <p>SIGN</p> <p>Blood Type</p>
+          <p>{props.birthday}</p>
+          <p>{props.zodiac}</p> <p>{props.blood}</p>
         </div>
         <div
           className="z-1 col-start-1 col-end-7 row-start-1 row-end-6
         flex justify-end"
         >
-          <div className=" member_mobileTopRightTriangle"></div>
+          <div className={props.mobileT}></div>
         </div>
       </div>
       <div className="w-[480px] m-auto lg:hidden">
@@ -62,37 +60,39 @@ const Member = (props) => {
           hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
           per conubia nostra inceptos himenaeos.
         </p>
-        <div className="w-[90%] h-[200px] bg-(--color-red) m-auto opacity-[70%]"></div>
+        <div
+          className={`w-[90%] h-[200px] ${props.scrollColor} m-auto opacity-[70%]`}
+        ></div>
       </div>
 
       <div className="hidden lg:block lg:w-[950px] xl:w-[1200px] m-auto">
         <div className="grid grid-cols-12 grid-rows-12 xl:grid-rows-10">
           <h2
-            className="z-2 col-start-1 col-end-7 row-start-1 row-end-3 ml-[0.25em]
-          uppercase text-(--color-red) font-(family-name:--font-solid) 
+            className={`z-2 col-start-1 col-end-7 row-start-1 row-end-3 ml-[0.25em]
+          uppercase ${props.color} font-(family-name:--font-solid) 
           text-[6.5rem] text-shadow-[4px_6px_0px_rgb(0,0,0,0.9)] tracking-wider
           
-          xl:text-[7rem] xl:tracking-widest xl:ml-[0.45em]"
+          xl:text-[7rem] xl:tracking-widest xl:ml-[0.45em]`}
           >
-            renjun
+            {props.name}
           </h2>
           <img
-            src={props.isDark ? dm_renjunFull : lm_renjunFull}
+            src={props.IMG}
             alt=""
             className={`z-2 col-start-7 col-end-13 row-start-2 row-end-10
             justify-self-start self-center xl:justify-self-center
-            ${props.isDark ? "w-[380px]" : "w-[500px]"}
+            ${props.fullW}
             
             xl:row-start-1 xl:row-end-9`}
           />
           <div
-            className="z-2 col-start-1 col-end-7 row-start-3 row-end-4 self-center mr-[5em]
+            className="z-2 col-start-1 col-end-7 row-start-3 row-end-4 self-center mr-[8em]
         flex justify-evenly font-(family-name:--font-text) font-bold
         
         xl:row-start-2 xl:mr-[7em]"
           >
-            <p>Month 00 2000</p>
-            <p>SIGN</p> <p>Blood Type</p>
+            <p>{props.birthday}</p>
+            <p>{props.zodiac}</p> <p>{props.blood}</p>
           </div>
           <p
             className="z-2 col-start-1 col-end-7 row-start-4 row-end-10 
@@ -114,18 +114,17 @@ const Member = (props) => {
           </p>
           <div
             className={`z-1 ${
-              windowWidth >= 1280
-                ? "member_fullTopRightTriangle"
-                : "member_desktopTopRightTriangle"
+              windowWidth >= 1280 ? props.fullT : props.desktopT
             }
           col-start-6 col-end-13 row-start-1 row-end-13
           xl:row-end-11`}
           ></div>
           <div
-            className="z-2 col-start-1 col-end-11 row-start-10 row-end-13
-          w-[95%] h-[200px] bg-(--color-red) m-auto opacity-[70%]
+            className={`z-2 col-start-1 col-end-11 row-start-10 row-end-13
+          w-[95%] h-[200px] m-auto opacity-[70%]
+          ${props.scrollColor}
           
-          xl:row-start-8 xl:row-end-11 xl:h-[225px] xl:w-[90%]"
+          xl:row-start-8 xl:row-end-11 xl:h-[225px] xl:w-[90%]`}
           ></div>
         </div>
       </div>
