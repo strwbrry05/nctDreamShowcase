45e21033e52ab53450fb7a707be4cdc156180dc4
strwbrry05 <jq.strwbrry05@gmail.com>
2025-07-07 01:07:40 -0700

    dm member slides

--- src/components/AllMembers.jsx
+++ src/components/AllMembers.jsx
@@ -19,47 +19,137 @@ import lm_renjun2 from "../assets/images/lightMode/lm_renjun2.png";
 import lm_renjun3 from "../assets/images/lightMode/lm_renjun3.png";
 import lm_renjun4 from "../assets/images/lightMode/lm_renjun4.png";
 import lm_renjun5 from "../assets/images/lightMode/lm_renjun5.png";
+import dm_renjun1 from "../assets/images/darkMode/dm_renjun1.png";
+import dm_renjun2 from "../assets/images/darkMode/dm_renjun2.png";
+import dm_renjun3 from "../assets/images/darkMode/dm_renjun3.png";
+import dm_renjun4 from "../assets/images/darkMode/dm_renjun4.png";
+import dm_renjun5 from "../assets/images/darkMode/dm_renjun5.png";
 
 import lm_mark1 from "../assets/images/lightMode/lm_mark1.png";
 import lm_mark2 from "../assets/images/lightMode/lm_mark2.png";
 import lm_mark3 from "../assets/images/lightMode/lm_mark3.png";
 import lm_mark4 from "../assets/images/lightMode/lm_mark4.png";
 import lm_mark5 from "../assets/images/lightMode/lm_mark5.png";
+import dm_mark1 from "../assets/images/darkMode/dm_mark1.png";
+import dm_mark2 from "../assets/images/darkMode/dm_mark2.png";
+import dm_mark3 from "../assets/images/darkMode/dm_mark3.png";
+import dm_mark4 from "../assets/images/darkMode/dm_mark4.png";
+import dm_mark5 from "../assets/images/darkMode/dm_mark5.png";
 
 import lm_jeno1 from "../assets/images/lightMode/lm_jeno1.png";
 import lm_jeno2 from "../assets/images/lightMode/lm_jeno2.png";
 import lm_jeno3 from "../assets/images/lightMode/lm_jeno3.png";
 import lm_jeno4 from "../assets/images/lightMode/lm_jeno4.png";
 import lm_jeno5 from "../assets/images/lightMode/lm_jeno5.png";
+import dm_jeno1 from "../assets/images/darkMode/dm_jeno1.png";
+import dm_jeno2 from "../assets/images/darkMode/dm_jeno2.png";
+import dm_jeno3 from "../assets/images/darkMode/dm_jeno3.png";
+import dm_jeno4 from "../assets/images/darkMode/dm_jeno4.png";
+import dm_jeno5 from "../assets/images/darkMode/dm_jeno5.png";
 
 import lm_haechan1 from "../assets/images/lightMode/lm_haechan1.png";
 import lm_haechan2 from "../assets/images/lightMode/lm_haechan2.png";
 import lm_haechan3 from "../assets/images/lightMode/lm_haechan3.png";
 import lm_haechan4 from "../assets/images/lightMode/lm_haechan4.png";
 import lm_haechan5 from "../assets/images/lightMode/lm_haechan5.png";
+import dm_haechan1 from "../assets/images/darkMode/dm_haechan1.png";
+import dm_haechan2 from "../assets/images/darkMode/dm_haechan2.png";
+import dm_haechan3 from "../assets/images/darkMode/dm_haechan3.png";
+import dm_haechan4 from "../assets/images/darkMode/dm_haechan4.png";
+import dm_haechan5 from "../assets/images/darkMode/dm_haechan5.png";
 
 import lm_jaemin1 from "../assets/images/lightMode/lm_jaemin1.png";
 import lm_jaemin2 from "../assets/images/lightMode/lm_jaemin2.png";
 import lm_jaemin3 from "../assets/images/lightMode/lm_jaemin3.png";
 import lm_jaemin4 from "../assets/images/lightMode/lm_jaemin4.png";
 import lm_jaemin5 from "../assets/images/lightMode/lm_jaemin5.png";
+import dm_jaemin1 from "../assets/images/darkMode/dm_jaemin1.png";
+import dm_jaemin2 from "../assets/images/darkMode/dm_jaemin2.png";
+import dm_jaemin3 from "../assets/images/darkMode/dm_jaemin3.png";
+import dm_jaemin4 from "../assets/images/darkMode/dm_jaemin4.png";
+import dm_jaemin5 from "../assets/images/darkMode/dm_jaemin5.png";
 
 import lm_chenle1 from "../assets/images/lightMode/lm_chenle1.png";
 import lm_chenle2 from "../assets/images/lightMode/lm_chenle2.png";
 import lm_chenle3 from "../assets/images/lightMode/lm_chenle3.png";
 import lm_chenle4 from "../assets/images/lightMode/lm_chenle4.png";
 import lm_chenle5 from "../assets/images/lightMode/lm_chenle5.png";
+import dm_chenle1 from "../assets/images/darkMode/dm_chenle1.png";
+import dm_chenle2 from "../assets/images/darkMode/dm_chenle2.png";
+import dm_chenle3 from "../assets/images/darkMode/dm_chenle3.png";
+import dm_chenle4 from "../assets/images/darkMode/dm_chenle4.png";
+import dm_chenle5 from "../assets/images/darkMode/dm_chenle5.png";
 
 import lm_jisung1 from "../assets/images/lightMode/lm_jisung1.png";
 import lm_jisung2 from "../assets/images/lightMode/lm_jisung2.png";
 import lm_jisung3 from "../assets/images/lightMode/lm_jisung3.png";
 import lm_jisung4 from "../assets/images/lightMode/lm_jisung4.png";
 import lm_jisung5 from "../assets/images/lightMode/lm_jisung5.png";
+import dm_jisung1 from "../assets/images/darkMode/dm_jisung1.png";
+import dm_jisung2 from "../assets/images/darkMode/dm_jisung2.png";
+import dm_jisung3 from "../assets/images/darkMode/dm_jisung3.png";
+import dm_jisung4 from "../assets/images/darkMode/dm_jisung4.png";
+import dm_jisung5 from "../assets/images/darkMode/dm_jisung5.png";
 
 import Member from "./Member";
 
 const AllMembers = (props) => {
   // dark mode arrays
+  const DrenjunSlides = [
+    { id: 1, src: dm_renjun1, alt: "renjun slide 1" },
+    { id: 2, src: dm_renjun2, alt: "renjun slide 2" },
+    { id: 3, src: dm_renjun3, alt: "renjun slide 3" },
+    { id: 4, src: dm_renjun4, alt: "renjun slide 4" },
+    { id: 5, src: dm_renjun5, alt: "renjun slide 5" },
+  ];
+
+  const DmarkSlides = [
+    { id: 1, src: dm_mark1, alt: "mark slide 1" },
+    { id: 2, src: dm_mark2, alt: "mark slide 2" },
+    { id: 3, src: dm_mark3, alt: "mark slide 3" },
+    { id: 4, src: dm_mark4, alt: "mark slide 4" },
+    { id: 5, src: dm_mark5, alt: "mark slide 5" },
+  ];
+
+  const DjenoSlides = [
+    { id: 1, src: dm_jeno1, alt: "jeno slide 1" },
+    { id: 2, src: dm_jeno2, alt: "jeno slide 2" },
+    { id: 3, src: dm_jeno3, alt: "jeno slide 3" },
+    { id: 4, src: dm_jeno4, alt: "jeno slide 4" },
+    { id: 5, src: dm_jeno5, alt: "jeno slide 5" },
+  ];
+
+  const DhaechanSlides = [
+    { id: 1, src: dm_haechan1, alt: "haechan slide 1" },
+    { id: 2, src: dm_haechan2, alt: "haechan slide 2" },
+    { id: 3, src: dm_haechan3, alt: "haechan slide 3" },
+    { id: 4, src: dm_haechan4, alt: "haechan slide 4" },
+    { id: 5, src: dm_haechan5, alt: "haechan slide 5" },
+  ];
+
+  const DjaeminSlides = [
+    { id: 1, src: dm_jaemin1, alt: "jaemin slide 1" },
+    { id: 2, src: dm_jaemin2, alt: "jaemin slide 2" },
+    { id: 3, src: dm_jaemin3, alt: "jaemin slide 3" },
+    { id: 4, src: dm_jaemin4, alt: "jaemin slide 4" },
+    { id: 5, src: dm_jaemin5, alt: "jaemin slide 5" },
+  ];
+
+  const DchenleSlides = [
+    { id: 1, src: dm_chenle1, alt: "chenle slide 1" },
+    { id: 2, src: dm_chenle2, alt: "chenle slide 2" },
+    { id: 3, src: dm_chenle3, alt: "chenle slide 3" },
+    { id: 4, src: dm_chenle4, alt: "chenle slide 4" },
+    { id: 5, src: dm_chenle5, alt: "chenle slide 5" },
+  ];
+
+  const DjisungSlides = [
+    { id: 1, src: dm_jisung1, alt: "jisung slide 1" },
+    { id: 2, src: dm_jisung2, alt: "jisung slide 2" },
+    { id: 3, src: dm_jisung3, alt: "jisung slide 3" },
+    { id: 4, src: dm_jisung4, alt: "jisung slide 4" },
+    { id: 5, src: dm_jisung5, alt: "jisung slide 5" },
+  ];
 
   // light mode arrays
   const renjunSlides = [
@@ -127,7 +217,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={markSlides}
+          slides={props.isDark ? DmarkSlides : markSlides}
           color={
             props.isDark ? "text-(--color-yellow)" : "text-(--color-yellow)"
           }
@@ -176,7 +266,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={renjunSlides}
+          slides={props.isDark ? DrenjunSlides : renjunSlides}
           color={props.isDark ? "text-(--color-red)" : "text-(--color-red)"}
           scrollColor={
             props.isDark ? "bg-(--color-member-dark)" : "bg-(--color-red)"
@@ -223,7 +313,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={jenoSlides}
+          slides={props.isDark ? DjenoSlides : jenoSlides}
           color={
             props.isDark ? "text-(--color-dark-red)" : "text-(--color-blue)"
           }
@@ -278,7 +368,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={haechanSlides}
+          slides={props.isDark ? DhaechanSlides : haechanSlides}
           color={
             props.isDark ? "text-(--color-yellow)" : "text-(--color-yellow)"
           }
@@ -327,7 +417,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={jaeminSlides}
+          slides={props.isDark ? DjaeminSlides : jaeminSlides}
           color={props.isDark ? "text-(--color-red)" : "text-(--color-red)"}
           scrollColor={
             props.isDark ? "bg-(--color-member-dark)" : "bg-(--color-red)"
@@ -372,7 +462,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={chenleSlides}
+          slides={props.isDark ? DchenleSlides : chenleSlides}
           color={
             props.isDark ? "text-(--color-dark-red)" : "text-(--color-blue)"
           }
@@ -431,7 +521,7 @@ const AllMembers = (props) => {
         }`}
       >
         <Member
-          slides={jisungSlides}
+          slides={props.isDark ? DjisungSlides : jisungSlides}
           color={
             props.isDark ? "text-(--color-yellow)" : "text-(--color-yellow)"
           }
